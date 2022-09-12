3c7948a6d341286fca84e3b3483572f7a3b65cec
ELMATIYOUSSEF <ysf.elmati@gmail.com>
2022-09-12 09:28:58 +0100

    chalange_1 de la partie function

---
+++ challange_function/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	 // function add
+	 add(2,10);
+}
+//function faire l'addition des deux nombres
+void add(int a, int b) {
+	int c ;
+	c=a+b ;
+	printf("%d + %d = %d",a,b,c);
+}

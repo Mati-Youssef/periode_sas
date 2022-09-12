08f1bee2d1bd6f04f18a721740ce7401ec39f364
ELMATIYOUSSEF <ysf.elmati@gmail.com>
2022-09-12 09:47:34 +0100

    organiser le challenge 2

--- challange_function/challange2/main.c
+++ challange_function/challange2/main.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 int main(){
+	// function echange 
 	echange(2,3);
 }
-
+// function pour  échanger la valeur de a avec la valeur de b
 void echange(int a, int b){
 	int c=0;
 	c=a ;

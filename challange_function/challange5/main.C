9f9bd776d3165f3e618e59367ee62a00f8d5afa4
ELMATIYOUSSEF <ysf.elmati@gmail.com>
2022-09-12 14:16:17 +0100

    challange 5 a la partie de function

---
+++ challange_function/challange5/main.C
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Challenge 11 : Année Bissextile
+
+//""""""""""""""""""""""""""
+// function is_leap_year()
+bool is_leap_year(int anne)
+{
+ bool test ;
+ if( (anne%4==0 && anne%100!=0) || anne%400 ==0)
+ {
+	 	test = true ;
+ } 
+ else
+ {
+        test =false ;	
+ } 
+  return test ; 
+  
+}
+//''''''''''''''''''''''''''''''''''
+// function main 
+//''''''''''''''''''''''''''''''''''
+
+int main()
+{
+ int year;
+ bool test ;
+ printf(" saisir une annee entre 1800 et 10000 \n ");
+ scanf("%d",&year);
+ test = is_leap_year(year);
+ if (test==true){
+ 	printf(" %d est bissextile ",year);
+ }
+ else 
+ {
+    printf("%d ne pas bissextile",year);
+ }
+}
+
